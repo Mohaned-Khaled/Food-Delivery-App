e6de4412e75455ca79a9b818bc0b20cc48189516
Mohaned-Khaled <mohanedkhf98@gmail.com>
2022-12-04 22:38:13 +0200

    uploading

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBna8ShOwRLS567Qjjz_jPr216g_VpkBjE",
-  authDomain: "restaurantapp-4471f.firebaseapp.com",
-  databaseURL: "https://restaurantapp-4471f-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-4471f",
-  storageBucket: "restaurantapp-4471f.appspot.com",
-  messagingSenderId: "1097681975686",
-  appId: "1:1097681975686:web:3d802979dd330115d05407",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
